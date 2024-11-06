f3e3f634247e6b77042d417c500219450bb131ed
HolbyKate <6804@holbertonstudents.com>
2024-11-06 22:26:38 +0100

    Print 3 times a string and only part of it

---
+++ 0x00-hello-dart/4-print_string.dart
@@ -0,0 +1,6 @@
+void main() {
+    String str = "Holberton School";
+    print('${str * 3}');// Multiply the string
+    print(str.substring(0, 9));// Take only 9 letters
+    print('');//Print newline only
+}
