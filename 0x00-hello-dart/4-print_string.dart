01bc299e7e984664c67a1f0c1553ba8f80cc0e47
HolbyKate <6804@holbertonstudents.com>
2024-11-06 23:58:19 +0100

    Fix adding new line in substring line

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,6 +1,5 @@
 void main() {
     String str = "Holberton School";
     print('${str * 3}');// Multiply the string
-    print(str.substring(0, 9));// Take only 9 letters
-    print('');//Print newline only
+    print('${str.substring(0, 9)}\n');// Take only 9 letters
 }
