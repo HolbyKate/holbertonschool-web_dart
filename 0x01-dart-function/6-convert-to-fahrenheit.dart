f6985fd3bde7f9a7b3cdd3d4a4e63f4660fa71d0
HolbyKate <6804@holbertonstudents.com>
2024-11-08 16:22:05 +0100

    Fix: adding toList to convert map result

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -2,9 +2,11 @@
 // in celsius, converts them to fahrenheit and returns them in a list.
 List<double> convertToF(List<double> temperaturesInC) {
     // Using map method
-    return temperaturesInC.map(celsius) {
+    return temperaturesInC.map((celsius) {
         // Make calcul to convert in fahrenheit
         double fahrenheit = (celsius * 9 / 5) + 32;
+        // Return temperature with 2 decimal
         return double.parse(fahrenheit.toStringAsFixed(2));
-}
+        //Adding toList to convert map result
+        }).toList();
 }
