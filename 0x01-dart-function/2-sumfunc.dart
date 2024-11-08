6317ec5f63134f24ab265b357efb5f1f8831255a
HolbyKate <6804@holbertonstudents.com>
2024-11-08 10:39:57 +0100

    Write three functions add, sub and message

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,17 @@
+// function that accepts 2 args and return addition
+int add(int a, int b) {
+    return (a + b);
+}
+
+// function that accepts 2 args and return substraction
+int sub(int a, int b) {
+    return (a - b);
+}
+
+// function that accepts 2 args and return message with output of 2 other func
+String showFunc(int a, int b) {
+    int addition = add(a, b);
+    int substraction = sub(a, b);
+
+    return ('Add $a + $b = $addition \nSub $a - $b = $substraction');
+}
