b732250097e39e5fbb351ce9eb73488ee6bd83b7
HolbyKate <6804@holbertonstudents.com>
2024-11-08 15:23:06 +0100

    Print formatted message with outer and inner function

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,14 @@
 void outer(String name, String id) {
-
+    // Call the inner function and store the result
+    String innerResult = inner();
+    //Split in 2 parts
+    List<String> nameParts = name.split(' ');
+    String firstName = nameParts[0];
+    String lastInitial = nameParts[1][0];
+    print('$innerResult $lastInitial.$firstName your id is $id');
 }
 
+// Return string before the split part and id
 String inner() {
-    
+    return ('Hello Agent');
 }
