1a8db2f28dfb9b926153e0fb798cad1a8eac983c
HolbyKate <6804@holbertonstudents.com>
2024-11-18 14:53:08 +0100

    Add function to find longest unique substring in Dart using List and index

---
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -0,0 +1,14 @@
+// Return the longest no repeating substring
+
+String longestUniqueSubstring(String str); {
+    List<String, chars = str.split('');
+    String current = '';
+    String longest = '';
+
+// Using string operation
+  for (String char in chars) {
+    current = current.substring(current.indexOf(char) + 1) + char;
+    if (current.length > longest.length) longest = current;
+  }
+  return longest;
+}
