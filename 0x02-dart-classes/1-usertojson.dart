c7214be8477ecb9e08d0b213b63caefe32c80e9a
HolbyKate <6804@holbertonstudents.com>
2024-11-18 17:44:16 +0100

    Create an user class with toJson method

---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,15 @@
+// Create an user class with toJson method
+
+class User {
+    final String name;
+    final int age;
+    final double height;
+
+    User({required this.name, required this.age, required this.height});
+
+    Map<String, dynamic> toJson() => {
+        'name': name,
+        'age': age,
+        'height': height
+    };
+}
