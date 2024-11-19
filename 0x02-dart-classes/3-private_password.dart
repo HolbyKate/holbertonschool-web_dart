594493bbc2de11cfcab50dc8277f2d7152c242f8
HolbyKate <6804@holbertonstudents.com>
2024-11-19 09:05:10 +0100

    Adding required this password to make it private

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,39 @@
+// Create a class called Password and give it a string property called password
+
+class Password {
+  String password = "";
+
+  // Constructor
+  Password({required this.password});
+
+  // Method to check if the password is valid
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+
+    // Check uppercase letters
+    bool hasUpperCase = password.contains(RegExp(r'[A-Z]'));
+    if (!hasUpperCase) {
+      return false;
+    }
+
+    // Check lowercase letters
+    bool hasLowerCase = password.contains(RegExp(r'[a-z]'));
+    if (!hasLowerCase) {
+      return false;
+    }
+
+    // Check numbers
+    bool hasNumbers = password.contains(RegExp(r'[0-9]'));
+    if (!hasNumbers) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $password";
+  }
+}
