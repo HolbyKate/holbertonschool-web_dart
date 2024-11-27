304bcd7a76a9d3d902f4c4f5421df32b2ce68f53
HolbyKate <6804@holbertonstudents.com>
2024-11-27 08:41:27 +0100

    Adding id in map

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -8,7 +8,7 @@ class User {
 
   User({required this.id, required this.name, required this.age, required this.height});
 
-  Map<String, dynamic> toJson() => {'name': name, 'age': age, 'height': height};
+  Map<String, dynamic> toJson() => {'id': id, 'name': name, 'age': age, 'height': height};
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
         id: userJson['id'],
