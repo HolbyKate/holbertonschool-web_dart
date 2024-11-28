2d68fbbdff112e82531a546a3f8fdf746d5e68c1
HolbyKate <6804@holbertonstudents.com>
2024-11-28 15:51:47 +0100

    Change all

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,41 +3,20 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    bool foundRick = false;
-    bool foundAnnie = false;
+    var url = 'https://rickandmortyapi.com/api/character';
+    final response = await http.get(Uri.parse(url));
 
-    String url = 'https://rickandmortyapi.com/api/character';
-
-    while (url != '' && !foundAnnie) {
-      final response = await http.get(Uri.parse(url));
-
-      if (response.statusCode != 200) {
-        throw Exception('Failed to load characters');
-      }
-
-      final data = jsonDecode(response.body);
-      final List characters = data['results'];
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      List characters = jsonResponse['results'];
 
       for (var character in characters) {
-        String name = character['name'];
-
-        if (name == "Rick Sanchez") {
-          foundRick = true;
-        }
-
-        if (foundRick) {
-          print(name);
-        }
-
-        if (name == "Annie") {
-          foundAnnie = true;
-          break;
-        }
+        print(character['name']);
       }
-
-      url = data['info']['next'] ?? '';
+    } else {
+      throw Exception('Failed to load characters');
     }
-  } catch (e) {
-    print('error caught: $e');
+  } catch (err) {
+    print('error caught: $err');
   }
 }
