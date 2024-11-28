0c51d3aa9c2d67d753bd5e5fbcb0ecf83d44dad1
HolbyKate <6804@holbertonstudents.com>
2024-11-28 11:49:58 +0100

    Get the user data to calculate total price

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,25 @@
+// Create function that calculates the total price of items for a certain user.
+import '4-util.dart';
+
+Future calculateTotal() async {
+  try {
+    // Get the user data
+    final userData = await fetchUserData();
+    final userInfo = json.decode(userData);
+    final String userId = userInfo['id'];
+
+    // Get the user order
+    final ordersData = await fetchUserOrders(userId);
+    final List<dynamic> orders = json.decode(ordersData);
+
+    // Calculate total price
+    double total = 0;
+    for (String product in orders) {
+      final priceData = await fetchProductPrice(product);
+      final double price = json.decode(priceData);
+      total += price;
+    }
+
+    return total;
+  }
+}
